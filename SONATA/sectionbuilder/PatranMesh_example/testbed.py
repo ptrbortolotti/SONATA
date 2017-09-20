68742a56afca89584b7e8773d73f27159b5931b3
TPflumm <tobias.pflumm@tum.de>
2017-09-20 15:01:20 +0200

    Removed ignored files

--- SONATA/sectionbuilder/PatranMesh_example/testbed.py
+++
@@ -1,8 +0,0 @@
-filename='SimpleMeshFile9.dat'
-a = ''
-with open(filename) as f:
-    for line in f:
-        line = line.partition('#')[0]
-        line = line.rstrip()
-        a += line 
-        a += '\n'
