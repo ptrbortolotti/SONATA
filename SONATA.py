26840983a5ae224fd15da98f5270a155e93bad7d
TPflumm <tobias.pflumm@tum.de>
2019-01-23 15:56:47 +0100

    changes in the highes level. Much more will come soon.

--- SONATA.py
+++ SONATA.py
@@ -1,39 +1,30 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-SONATA-CBM is a Preprocessor for Parametric Composite Rotor Blade 
-Cross-Sections.
+SONATA: Multidiciplinary Rotor Blade Design Environment for Structural 
+Optimization and Aeroelastic Analysis
+
 The definition of the rotor blade topology is deliberately associated to the 
 production of composite rotor blades. Thus, manufacturability is inherent from 
 the geometric layup definition. Using orthogonal projection with corner-style 
 differentiation the cross-section is discretized and can processed by the 
 Variational Asymptotic Beam Sectional Analysis (VABS) afterwards. 
 
-Date: 30/10/2018
-@author: T.Pflumm, W.Garre
+Date: 01/21/2019
+@author: T.Pflumm, W.Garre, P.Bortolotti
 
 Please Use the following Docstring styleguide:
 https://numpydoc.readthedocs.io/en/latest/format.html
 
 """
-import numpy as np
-from SONATA.cbm.fileIO.configuration import Configuration
-from SONATA.cbm.sonata_cbm import CBM
 
-fname = 'jobs/VariSpeed/uh60a_cbm_advanced/sec_config.yml'
-#fname = 'jobs/VariSpeed/uh60a_cbm_simple/sec_config.yml'
-#fname = 'jobs/AREA/R250/sec_config.yml'
-#fname = 'jobs/PBortolotti/sec_config.yml'
-config = Configuration(fname)
 
-job = CBM(config)
-
-job.cbm_gen_topo()
-job.cbm_gen_mesh(split_quads=True)
-
-job.cbm_review_mesh()
-job.cbm_post_2dmesh(title='Hello World!', plotTheta11=True)
-
-job.cbm_run_vabs()
-job.cbm_run_anbax()
-
-#job.cbm_post_3dtopo()
+    with open('jobs/PBortolotti/IEAonshoreWT.yaml', 'r') as myfile:
+        inputs  = myfile.read()
+    with open('jobs/PBortolotti/IEAturbine_schema.yaml', 'r') as myfile:
+        schema  = myfile.read()
+    validate(yaml.load(inputs), yaml.load(schema))
+    wt_data     = yaml.load(inputs)
+    
+    
+    Windturbine = Windturbine(readIAE37)
--- SONATA/classWindturbine.py
+++ SONATA/classWindturbine.py
@@ -7,10 +7,14 @@ Created on Wed Dec 19 09:37:42 2018
 """
 from jsonschema import validate
 import yaml
+import os
 
-from classPolar import Polar
-from classAirfoil import Airfoil
-from classMaterial import read_IAE37_materials
+if __name__ == '__main__':
+    os.chdir('..')
+
+from SONATA.classPolar import Polar
+from SONATA.classAirfoil import Airfoil
+from SONATA.classMaterial import read_IAE37_materials
 
 class Windturbine(object):
 
@@ -28,10 +32,12 @@ class Windturbine(object):
         if isinstance(name, str) and not 'NONAME': 
             self.name = name
 
+
     def __repr__(self):
         """__repr__ is the built-in function used to compute the "official" 
         string reputation of an object, """
         return 'Windturbine: '+ self.name
+    
     
     def read_IAE37(self, yml, yml_schema):
         """
@@ -41,16 +47,17 @@ class Windturbine(object):
         validate(yml, yml_schema)
         self.name = yml.get('name')
         self.airfoils = {af.get('name'):Airfoil(af) for af in yml.get('airfoils')}
-        self.materials = {m.name:m for m in read_IAE37_materials(yml.get('materials'))}
+        self.materials = read_IAE37_materials(yml.get('materials'))
+
 
     def plot_airfoilpolars():
         for af in wt.airfoils.values():
             af.plot_polars()
 
-if __name__ == '__main__':
-    import os
-    os.chdir('..')
-    
+
+
+
+if __name__ == '__main__':   
     with open('jobs/PBortolotti/IEAonshoreWT.yaml', 'r') as myfile:
         inputs  = myfile.read()
     with open('jobs/PBortolotti/IEAturbine_schema.yaml', 'r') as myfile:
