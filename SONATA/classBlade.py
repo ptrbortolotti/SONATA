b06da306b9d200b675cddf57e3541431ab54ece5
TPflumm <tobias.pflumm@tum.de>
2019-02-01 16:12:56 +0100

    updates to ClassBlade

--- SONATA/classBlade.py
+++ SONATA/classBlade.py
@@ -33,12 +33,11 @@ from SONATA.cbm.display.display_utils import export_to_JPEG, export_to_PNG, expo
                                         show_coordinate_system, display_SONATA_SegmentLst,\
                                         display_custome_shape, transform_wire_2to3d  
 
-
-
-
 class Blade(Component):
 
-    __slots__ = ('coordinates', 'chord', 'twist', 'blade_matrix', 'airfoilLst', 'sections')
+    __slots__ = ('coordinates', 'chord', 'twist', 'pitch_axis', 'blade_matrix', 'airfoilLst',  \
+                 'sections', 'f_chord', 'f_twist', 'f_coordinates_x',  \
+                 'f_coordinates_y', 'f_coordinates_z', 'f_pa')
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args,**kwargs)
@@ -76,6 +75,7 @@ class Blade(Component):
             lin. interpolated airfoils. 
         
         """
+        print('STATUS:\t Reading IAE37 Definition for Blade: %s' % (self.name))
         #Read information from DataDictionary
         tmp_coords = {}
         tmp = byml.get('bem_aero')
@@ -84,6 +84,7 @@ class Blade(Component):
         tmp_coords['z'] = np.asarray((tmp.get('coordinates').get('z').get('grid'),tmp.get('coordinates').get('z').get('values'))).T
         tmp_tw = np.asarray((tmp.get('twist').get('grid'),tmp.get('twist').get('values'))).T
         tmp_chord = np.asarray((tmp.get('chord').get('grid'),tmp.get('chord').get('values'))).T
+        tmp_pa = np.asarray((tmp.get('pitch_axis').get('grid'),tmp.get('pitch_axis').get('values'))).T
         airfoil_position = (tmp.get('airfoil_position').get('grid'),tmp.get('airfoil_position').get('labels'))
         
         #Generate Blade Matrix 
@@ -92,29 +93,30 @@ class Blade(Component):
             tmp.append(next((x for x in airfoils if x.name == an), None).id)
         arr = np.asarray([airfoil_position[0],tmp]).T
             
-        f_chord = interp1d(tmp_chord[:,0], tmp_chord[:,1], bounds_error=False, fill_value='extrapolate')
-        f_twist = interp1d(tmp_tw[:,0], tmp_tw[:,1], bounds_error=False, fill_value='extrapolate')
-        f_coordinates_x = interp1d(tmp_coords['x'][:,0], tmp_coords['x'][:,1], bounds_error=False, fill_value='extrapolate')
-        f_coordinates_y = interp1d(tmp_coords['y'][:,0], tmp_coords['y'][:,1], bounds_error=False, fill_value='extrapolate')
-        f_coordinates_z = interp1d(tmp_coords['z'][:,0], tmp_coords['z'][:,1], bounds_error=False, fill_value='extrapolate')
+        self.f_chord = interp1d(tmp_chord[:,0], tmp_chord[:,1], bounds_error=False, fill_value='extrapolate')
+        self.f_twist = interp1d(tmp_tw[:,0], tmp_tw[:,1], bounds_error=False, fill_value='extrapolate')
+        self.f_coordinates_x = interp1d(tmp_coords['x'][:,0], tmp_coords['x'][:,1], bounds_error=False, fill_value='extrapolate')
+        self.f_coordinates_y = interp1d(tmp_coords['y'][:,0], tmp_coords['y'][:,1], bounds_error=False, fill_value='extrapolate')
+        self.f_coordinates_z = interp1d(tmp_coords['z'][:,0], tmp_coords['z'][:,1], bounds_error=False, fill_value='extrapolate')
+        self.f_pa = interp1d(tmp_pa[:,0], tmp_pa[:,1], bounds_error=False, fill_value='extrapolate')
         
-    
         cs_pos = np.asarray([cs.get('position') for cs in byml.get('2d_fem').get('sections')])
-        x = np.unique(np.sort(np.hstack((tmp_chord[:,0], tmp_tw[:,0], tmp_coords['x'][:,0], tmp_coords['y'][:,0], tmp_coords['z'][:,0], arr[:,0], cs_pos))))
+        x = np.unique(np.sort(np.hstack((tmp_chord[:,0], tmp_tw[:,0], tmp_coords['x'][:,0], tmp_coords['y'][:,0], tmp_coords['z'][:,0], tmp_pa[:,0], arr[:,0], cs_pos))))
         
-        self.blade_matrix = np.transpose(np.unique(np.array([x, f_coordinates_x(x), f_coordinates_y(x), f_coordinates_z(x), f_chord(x), f_twist(x)]), axis=1))
+        self.blade_matrix = np.transpose(np.unique(np.array([x, self.f_coordinates_x(x), self.f_coordinates_y(x), self.f_coordinates_z(x), self.f_chord(x), self.f_twist(x), self.f_pa(x)]), axis=1))
         self.airfoilLst = [interp_airfoil_position(airfoil_position, airfoils, x) for x in x]
         self.coordinates = self.blade_matrix[:,0:4]
         self.chord = self.blade_matrix[:,[0,4]]
         self.twist = self.blade_matrix[:,[0,5]]
-                
+        self.pitch_axis = self.blade_matrix[:,[0,6]]         
+        
         #get sections information and init the CBM instances.
         tmp = byml.get('2d_fem').get('sections')
         self.sections = {}
-        for cs in tmp:
+        for cs in tmp:            
             cbm_config = CBMConfig(cs, materials, iae37=True)
             x = cs.get('position')
-            bm = np.array([x, f_coordinates_x(x), f_coordinates_y(x), f_coordinates_z(x), f_chord(x), f_twist(x)])
+            bm = np.array([x, self.f_coordinates_x(x), self.f_coordinates_y(x), self.f_coordinates_z(x), self.f_chord(x), self.f_twist(x), self.f_pa(x)])
             af = interp_airfoil_position(airfoil_position, airfoils, x)
             self.sections[x] = CBM(cbm_config, materials = materials, blade_matrix = bm, airfoil=af)
             #get blade_matrix and airfoil at position and pass to CBM init instance!
@@ -124,7 +126,7 @@ class Blade(Component):
 
     def plot_blade_matrix(self):
         """
-        illustrates the coordinates, chord, twist of the blade
+        plot the the coordinates, chord, twist of the blade
         """
         
         fig, ax = plt.subplots(3,2)
@@ -154,7 +156,7 @@ class Blade(Component):
         plt.show()
     
     
-    def gen_surface(self):
+    def post_3dtopo(self, flag_wf = True, flag_lft = False, flag_topo = True, flag_mesh = False, **kwargs):
         """
         generates the wireframe and the loft surface of the blade
 
@@ -166,23 +168,46 @@ class Blade(Component):
         wireframe : list
             list of every airfoil_wire scaled and rotated at every grid point
         """
+        display, start_display, add_menu, add_function_to_menu = init_display()
+        display.Context.SetDeviationAngle(1e-5) # 0.001 default. Be careful to scale it to the problem.
+        display.Context.SetDeviationCoefficient(1e-5) # 0.001 default. Be careful to scale it to the problem. 
+        bg_c = ((20,6,111),(200,200,200))
+        display.set_bg_gradient_color(bg_c[0][0],bg_c[0][1],bg_c[0][2],bg_c[1][0],bg_c[1][1],bg_c[1][2])
+        show_coordinate_system(display,4)
         
-        wireframe = []
-        for bm, afl in zip(self.blade_matrix, self.airfoilLst):
-            if afl.wire == None:
-                afl.gen_OCCtopo()
+        #display WIREFRAME
+        if flag_wf:
+            wireframe = []
+            for bm, afl in zip(self.blade_matrix, self.airfoilLst):
+                if afl.wire == None:
+                    afl.gen_OCCtopo()
+                wire = afl.wire
+                
+                wire = rotate_wire(wire, gp_Ax1(gp_Pnt(bm[6],0,0), gp_Dir(0,0,1)), -bm[5], copy=True)
+                wire = translate_wire(wire, gp_Pnt(bm[6],0,0), gp_Pnt(0,0,0))
+                wire = scale_wire(wire, gp_Pnt(0,0,0), bm[4])
+                
+                wire = rotate_wire(wire, gp_Ax1(gp_Pnt(0,0,0), gp_Dir(0,0,1)), -np.pi/2)
+                wire = rotate_wire(wire, gp_Ax1(gp_Pnt(0,0,0), gp_Dir(0,1,0)), -np.pi/2)
+                wire = translate_wire(wire, gp_Pnt(0,0,0), gp_Pnt(bm[1],bm[2],bm[3]))
+                
+                wireframe.append(wire)
+                display.DisplayShape(wire, color='WHITE', transparency=0.3)
             
-            wire = afl.wire
-            wire = rotate_wire(wire, gp_Ax1(gp_Pnt(0,0,0), gp_Dir(1,0,0)), np.pi/2)
-            wire = rotate_wire(wire, gp_Ax1(gp_Pnt(0,0,0), gp_Dir(0,0,1)), -np.pi/2)
-            wire = translate_wire(wire, gp_Pnt(0,0,0), gp_Pnt(0,0.25,0))
-            wire = scale_wire(wire, gp_Pnt(0,0,0), bm[4])
-            wire = rotate_wire(wire, gp_Ax1(gp_Pnt(0,0,0), gp_Dir(1,0,0)), bm[5])  
-            wire = translate_wire(wire, gp_Pnt(0,0,0), gp_Pnt(bm[1],bm[2],bm[3]))
-            wireframe.append(wire)
-            display.DisplayShape(wire, color='BLACK')
-            #loft = make_loft(wireframe[:12], ruled=False, tolerance=1e-3, continuity=1, check_compatibility=True)
-        return wireframe         
+        if flag_lft:
+            loft = make_loft(wireframe, ruled=False, tolerance=1e-3, continuity=1, check_compatibility=True)
+            #display.DisplayShape(loft)
+        
+        if flag_topo:
+            for k,cs in B.sections.items():
+                coord = self.f_coordinates_x(k), self.f_coordinates_y(k), self.f_coordinates_z(k)
+                display_SONATA_SegmentLst(display, cs.SegmentLst, coord, -np.pi/2, -np.pi/2)
+        
+        display.View_Iso()
+        display.FitAll()
+        start_display()   
+        
+        return None         
         
 
 if __name__ == '__main__':
@@ -190,12 +215,8 @@ if __name__ == '__main__':
         
     with open('jobs/PBortolotti/IEAonshoreWT.yaml', 'r') as myfile:
         inputs  = myfile.read()
-    with open('jobs/PBortolotti/IEAturbine_schema.yaml', 'r') as myfile:
-        schema  = myfile.read()
-    
+        
     yml = yaml.load(inputs)
-    yml_schema = yaml.load(schema)
-    
     airfoils = [Airfoil(af) for af in yml.get('airfoils')]
     materials = read_IAE37_materials(yml.get('materials'))
     
@@ -203,17 +224,13 @@ if __name__ == '__main__':
     B = Blade(name='TestBlade')
     B.read_IAE37(byml, airfoils, materials)
 
-    for cs in B.sections.values():
-        cs.cbm_gen_topo()
-        cs.cbm_gen_mesh()
-        cs.cbm_post_2dmesh()
+#    for key, cs in B.sections.items():
+#        print('STATUS:\t Building Section at grid location %s' % (key))
+#        cs.cbm_gen_topo()
+#        cs.cbm_gen_mesh()
+#        cs.cbm_run_vabs()
+#        cs.cbm_post_2dmesh()
 
-    # ====== DISPLAY ==============
-    display, start_display, add_menu, add_function_to_menu = init_display()
-    p = gp_Pnt(0,0,0)
-    display.DisplayShape(p)
-    show_coordinate_system(display,3)
-    B.gen_surface()
-    display.View_Iso()
-    display.FitAll()
-    start_display()   
+    #%%====== DISPLAY ==============
+    #B.sections[0].cbm_gen_topo()
+    B.post_3dtopo()
