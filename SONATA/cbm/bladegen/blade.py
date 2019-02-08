f69ce540798e7140856777b702e0dfa6ae6ab405
TPflumm <tobias.pflumm@tum.de>
2019-02-08 17:19:13 +0100

    cleanup

--- SONATA/cbm/bladegen/blade.py
+++ SONATA/cbm/bladegen/blade.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
 
 
 from SONATA.cbm.bladegen.airfoil import Airfoil
-from SONATA.blade_utl import make_loft
+from SONATA.utl.blade_utl import make_loft
 from SONATA.cbm.topo.wire_utils import rotate_wire, translate_wire,scale_wire, mirror_wire_pnt_dir
 from SONATA.cbm.display.display_utils import show_coordinate_system 
 from SONATA.cbm.fileIO.CADinput import BSplineLst_from_intersect_shape
