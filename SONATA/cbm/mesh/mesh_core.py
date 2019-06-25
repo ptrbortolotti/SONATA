fixed major bug that inverted the coordinate system, and added a scaling fix to enforce the area constraint for the triangulation

SONATA/cbm/mesh/mesh_core.py
@@ -21,7 +21,7 @@ from SONATA.cbm.mesh.cell import Cell
 
 #===================FUNCTIONALITIES==========================================
 
-def triangle_mesh(nodes,options):
+def triangle_mesh(array, options):
     '''The triangle_mesh function generates the triagular mesh within the 
     a_nodes polygon. It uses the Python Triangle module, which is a python 
     wrapper around Jonathan Richard Shewchuks two-dimensional quality mesh
@@ -31,7 +31,7 @@ def triangle_mesh(nodes,options):
     - http://www.cs.cmu.edu/~quake/triangle.html
         
     Args:
-        nodes: (list of nodes), the list contains all nodes that are on the
+        array: (array of nodes), the array contains all nodes that are on the
             innermost boundary of the generated topology. And are the boundary 
             for the triangulation
             
@@ -43,20 +43,15 @@ def triangle_mesh(nodes,options):
             as keys
     '''
     
-    points = []
-    for n in nodes:
-        points.append([n.Pnt2d.X(),n.Pnt2d.Y()])
-      
-    old_vertices = np.asarray(points)
     tmp = []
-    for i,v in enumerate(old_vertices):  
-        if i == len(old_vertices)-1:
+    for i,v in enumerate(array):  
+        if i == len(array)-1:
             tmp.append([i,0])
         else: tmp.append([i,i+1])
     segments_core = np.asarray(tmp)
         
     poly = {'vertices': None, 'segments': None}
-    poly['vertices'] = old_vertices
+    poly['vertices'] = array
     poly['segments'] = segments_core
     #plt.plot(old_vertices[:,0],old_vertices[:,1],'.-')
     mesh =  triangulate(poly, options)
@@ -111,25 +106,28 @@ def gen_core_cells(a_nodes, area=1.0,**kwargs):
         options = kwargs.get('options')
     else:
         if area<1.0:  
-            options = 'pq'    
+            #options = 'pq'   
+            scalefactor = np.sqrt(1/area)+0.1
+            area = area * scalefactor**2
+            #print('area:', area, 'scalefactor:', scalefactor)
+            options = 'pa%f' % (area)   #Somehow crashing!?
+        
         else:
+             scalefactor = 1
              options = 'pa%f' % (area)   #Somehow crashing!?
-             
-
-    mesh = triangle_mesh(a_nodes, options)  
-  
-    tmp = []
-    for n in a_nodes:
-        tmp.append([n.Pnt2d.X(),n.Pnt2d.Y(),n.id])
-    old_vertices = np.asarray(tmp)
     
+    old_vertices = np.asarray([[n.Pnt2d.X(),n.Pnt2d.Y(), n.id] for n in a_nodes])
+    mesh = triangle_mesh(old_vertices[:,:2]*scalefactor, options)  
+  
     c_nodes = []
     connector = []      #connector stores the information [tri_vertex_id, old_node_id]
     for tri_id,v in enumerate(mesh['vertices']):
+        #print(type(v))
+        v = v/scalefactor
         vec = np.linalg.norm(old_vertices[:,:2]-v,axis=1)
         idx = np.argmin(vec)
         value = vec[idx]
-        if value<=1e-8:
+        if value<=1e-5:
              #checks if the vertex exists allready in the a_nodes list
              connector.append([tri_id,old_vertices[idx,2]])
         else: 

SONATA/cbm/topo/offset.py
@@ -10,15 +10,11 @@ import matplotlib.pyplot as plt
 from .utils import calc_DCT_angles, P2Pdistance, isclose,unique_rows,Polygon_orientation
 
 
-def shp_parallel_offset(arrPts,dist,join_style=1):
+def shp_parallel_offset(arrPts,dist,join_style=1, side='right', res=16):
     #OFFSET ALGORITHM
     #join_style = 1#( 1:round,2:mitre,3:bevels)
-    
-    side = 'left'    
-    res = 16 #resolution
     closed = None
     
-
     #==============SHAPELY-OFFSET ALGORITHM====================================
     if P2Pdistance(arrPts[0],arrPts[-1])<=1e-6:
         closed = True
@@ -53,10 +49,11 @@ def shp_parallel_offset(arrPts,dist,join_style=1):
     #TODO: Be careful not to reverse Linestring!
     if closed == True:
         Orientation = Polygon_orientation(data)
-        if Orientation == False:
+        if Orientation == True:
             data = np.flipud(data)
-       
-    
+            
+    if closed == False: 
+        data = np.flipud(data)
     #==============Interpolate large linear spaces=============================
     seg_P2Plength = []
     cumm_length = 0
@@ -113,26 +110,28 @@ def shp_parallel_offset(arrPts,dist,join_style=1):
 #        if after<before:
 #            print 'non unique elements found',len(data)
 
-
+    #======PLOTTING for debugging
 #    fig = plt.figure()
-#    ax = fig.add_subplot(111)
+#    fig.add_subplot(111)
 #    plt.clf()         
 #    plt.plot(*arrPts.T, color='black', marker='.')
-#    
-#    for i, item in enumerate(data):
-#            plt.annotate(i, (item[0],item[1]), color='red')
-#    
 #    plt.plot(*arrPts[0].T, color='green', marker='o')
 #    plt.plot(*arrPts[-1].T, color='purple', marker='o')
-#    plt.plot(*arrPts.T,color='black',marker='.')
+#    
+#    for i, item in enumerate(arrPts):
+#        plt.annotate(i, (item[0],item[1]), color='black')
 #    
 #    plt.plot(*data.T, color='red', marker='.')
 #    plt.plot(*data[0].T, color='green', marker='>')
 #    plt.plot(*data[-1].T, color='purple', marker='>')
+#    
+#    for i, item in enumerate(data):
+#        plt.annotate(i, (item[0],item[1]), color='red')
+#    
 #    plt.axis('equal')  
 #    plt.show()   
-#    
-#   
+    
+
     return data
 
     

SONATA/classBlade.py
@@ -239,7 +239,7 @@ class Blade(Component):
             wireframe.append(wire)
             tes.append(te_pnt)
         
-        nPoints = 5000
+        nPoints = 8000
         if len(wireframe) > 1:
             tmp = []
             for w in wireframe:
@@ -268,8 +268,9 @@ class Blade(Component):
         te_pnt = te_pnt.Transformed(trsf)
         
         array = PntLst_to_npArray(PntLst)
-        array = np.flipud(array) 
-        BSplineLst = BSplineLst_from_dct(array[:,0:2], angular_deflection = 15, tol_interp=1e-6)
+        #array = np.flipud(array) 
+        #print(array)
+        BSplineLst = BSplineLst_from_dct(array[:,0:2], angular_deflection = 20, tol_interp=1e-6)
 
         BoundaryBSplineLst = set_BSplineLst_to_Origin2(BSplineLst, gp_Pnt2d(te_pnt.Coord()[0],te_pnt.Coord()[1]))
         return BoundaryBSplineLst
@@ -310,6 +311,7 @@ class Blade(Component):
         airfoil_position = (yml.get('outer_shape_bem').get('airfoil_position').get('grid'),yml.get('outer_shape_bem').get('airfoil_position').get('labels'))
         tmp = []
         for an in airfoil_position[1]: 
+            print(an)
             tmp.append(next((x for x in airfoils if x.name == an), None).id)
         arr = np.asarray([airfoil_position[0],tmp]).T
             