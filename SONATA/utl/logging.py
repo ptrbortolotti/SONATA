7b76c43309a7718ebb74a59c760ff23771a91f28
TPflumm <tobias.pflumm@tum.de>
2019-07-02 10:11:07 +0200

    created a logging module to hide the DEBUG messages from matplotlib and shapely

---
+++ SONATA/utl/logging.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  2 10:03:17 2019
+Hides the DEBUG MEssages! 
+@author: gu32kij
+"""
+
+import logging    
+        
+#matplotlib logger
+mpl_logger = logging.getLogger('matplotlib')
+mpl_logger.setLevel(logging.WARNING)
+  
+shp_logger = logging.getLogger('shapely')
+shp_logger.setLevel(logging.WARNING)
+
