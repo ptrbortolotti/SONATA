387c1135cfe603d9f9e6aa606d5dd07ffad555fc
TPflumm <tobias.pflumm@tum.de>
2020-03-11 09:49:43 +0100

    added the first setup.py, setup.cfg and requirements template

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+setup()
