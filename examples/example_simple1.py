ea5f2d22a0abaeab6dbf2d766faa81ed6a57e0a1
Tobias Pflumm <tobias.pflumm@tum.de>
2020-03-30 16:33:38 +0200

    updated examples

--- examples/example_simple1.py
+++ examples/example_simple1.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import numpy as np
 from SONATA.classBlade import Blade
 
-job = Blade(filename='UH-60A.yml')
-#job.blade_gen_section(mesh_flag = True, split_quads=False)
-#job.blade_plot_sections()
-job.blade_gen_loft(ruled=True, tolerance=1e-6, continuity=4, check_compatibility=True, filename="uh60a.iges")
+job = Blade(filename='simple_blade.yml')
+job.blade_gen_section(mesh_flag = True, split_quads=False)
+job.blade_plot_sections()
+job.blade_gen_loft(ruled=True, tolerance=1e-6, continuity=4, check_compatibility=True, filename="simple.iges")
 job.blade_post_3dtopo(flag_lft = True, flag_topo = True, flag_mesh = False)
 
-#job.blade_run_vabs(vabs_path = 'C:\Program Files (x86)\VABS\VABSIII.exe')
+loads = { "F" : np.array([[0, 0, 0, 0], 
+                         [1, 0, 0, 0]]),
+          "M" : np.array([[0, 0, 200, 0],
+                         [1, 0, 100, 0]]) }
+ 
+job.blade_run_vabs(loads, vabs_path='C:\Program Files (x86)\VABS\VABSIII')
+job.blade_plot_sections(attribute = "stressM.sigma11")
